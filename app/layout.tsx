d5ffa4648e6402db5f3e75f6d80b8afe6a4329bc
fuerte <leandrofuerte75@gmail.com>
2025-03-26 09:14:40 +0100

    Deleting theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="min-h-screen bg-gradient-to-br from-[#2b2726] to-[#0a405d]">{children}</div>
-        </ThemeProvider>
       </body>
     </html>
   )
