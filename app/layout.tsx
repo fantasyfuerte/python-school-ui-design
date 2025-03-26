b7c99398dca3b6a0b01957fb8941c04b00a9794c
fuerte <leandrofuerte75@gmail.com>
2025-03-26 09:33:06 +0100

    Fixing imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
-import type React from "react";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PythonPro Challenges",
+  title: "Python School",
   description: "Solve Python coding challenges and improve your skills",
 };
 
