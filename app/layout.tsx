07d4fd9b23d9b77032f0d2703c1b9515edcee8e1
fuerte <leandrofuerte75@gmail.com>
2025-03-26 09:30:22 +0100

    Importing header only in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PythonPro Challenges",
   description: "Solve Python coding challenges and improve your skills",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-          <div className="min-h-screen bg-gradient-to-br from-[#2b2726] to-[#0a405d]">{children}</div>
+        <div className="min-h-screen bg-gradient-to-br from-[#2b2726] to-[#0a405d]">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
-  )
+  );
 }
-
-
-
-import './globals.css'
