944389c386b7d9a3285a865ccfea4fe241c654a8
fuerte <leandrofuerte75@gmail.com>
2025-03-27 06:08:49 +0100

    Creating function scroll

--- components/button-start.tsx
+++ components/button-start.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+"use client";
+
 import { Button } from "./ui/button";
+
+function Scroll() {
+  window.scrollTo({
+    top: 400,
+    behavior: "smooth",
+  });
+}
 
 function ButtonStart() {
   return (
-    <Button className="mt-8 bg-cta1 hover:bg-cta1/90 text-backgroundgrey font-bold px-8 py-6 text-lg">
+    <Button
+      onClick={Scroll}
+      className="mt-8 bg-cta1 hover:bg-cta1/90 text-backgroundgrey font-bold px-8 py-6 text-lg"
+    >
       Start Coding Now
     </Button>
   );
